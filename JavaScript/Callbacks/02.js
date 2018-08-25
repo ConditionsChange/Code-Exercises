e15f97b55d2981b81c23b67e743e3c40a7afee30
conditionschange <kcruzdev@gmail.com>
2018-08-25 16:44:32 -0700

    added callbacks, async callbacks, orm examples, and express mvc examples

---
+++ JavaScript/Callbacks/02.js
@@ -0,0 +1,16 @@
+var subtract = function(numOne, numTwo) {
+  return numOne - numTwo;
+};
+
+var multiply = function(numOne, numTwo) {
+  return numOne * numTwo;
+};
+
+var addCallBacks = function(functionOne, functionTwo) {
+  return functionOne(6, 2) + functionTwo(6, 3);
+};
+
+// What does this return?
+console.log(addCallBacks(subtract, multiply));
+
+// Answer: 4 + 18 = 22
